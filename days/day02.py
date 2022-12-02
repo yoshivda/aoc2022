9fee5c421944f6f00962c596dbd39a6197a8ce03
Yoshi van den Akker <y.a.w.vandenakker@student.tudelft.nl>
2022-12-02 15:26:57 +0100

    Make day 2 shorter

--- days/day02.py
+++ days/day02.py
@@ -2,12 +2,12 @@ from lib import load_input
 
 
 def solve(data):
-    # return part_one(data)
-    return part_two(data)
+    return part_one(data)
+    # return part_two(data)
 
 
 def part_one(data):
-    return sum(ord(line[2]) - 87 + (6 if 'ABC'['ABC'.index(chr(ord(line[2]) - 23)) - 1] == line[0] else 3 if line[0] == chr(ord(line[2]) - 23) else 0) for line in data.splitlines())
+    return sum(ord(line[2]) - 87 + 3 * ((ord(line[2]) - 22 - ord(line[0])) % 3) for line in data.splitlines())
 
 
 def part_two(data):
